ce1e59912dff58f47523416896099386615a46a1
Joshua Bousel <josh@bousel.com>
2016-03-23 13:33:11 -0400

    Removed limit columns from events table

--- js/facilities-wrapper.js
+++ js/facilities-wrapper.js
@@ -4,5 +4,8 @@ Y.use("jquery-noconflict", function(Y) {
 		jQuery('.lo-CalendarDateValue a').click(function(e){
 			e.preventDefault();
 		});
+		
+		// Change
+		jQuery('#lo-ticket_class_list_user .lo-Event_lc_Cell_border_top').first().attr('colspan',2);
 	});
 });
