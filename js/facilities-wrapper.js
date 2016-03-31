86d0ac2a7ea09307e30b7bf0fb541bade8f3167e
Josh Bousel <Josh@wcss-mbp-2.wcs.org>
2016-03-31 14:32:55 -0400

    Added new event styles

--- js/facilities-wrapper.js
+++ js/facilities-wrapper.js
@@ -1,11 +1,14 @@
 Y.use("jquery-noconflict", function(Y) {
+
 	jQuery(function(){
 		// Prevent ticket event date links from going anywhere
 		jQuery('.lo-CalendarDateValue a').click(function(e){
 			e.preventDefault();
 		});
 		
-		// Change
-		jQuery('#lo-ticket_class_list_user .lo-Event_lc_Cell_border_top').first().attr('colspan',2);
+		// Change colspan when there's no tax-deductible info
+		if(jQuery('body').hasClass('wcs-event--no-tax-deductible')) {
+			jQuery('#lo-ticket_class_list_user .lo-Event_lc_Cell_border_top').first().attr('colspan',2);	
+		}
 	});
 });
